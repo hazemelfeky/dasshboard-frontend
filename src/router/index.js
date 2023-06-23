ac1f42481414cb1dc5fa27e4d252f72de50d3ce0
hazemelfeky <hazemelfeky25@gmail.com>
2023-06-24 01:58:48 +0300

    feat: add router guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,68 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import SplashView from '@/views/SplashView.vue'
-import LoginView from '@/views/LoginView.vue'
-import DashboardView from '@/views/DashboardView.vue'
-import TerminalView from '@/views/TerminalView.vue'
-import DockerView from '@/views/DockerView.vue'
-import ExplorerView from '@/views/ExplorerView.vue'
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import SplashView from "@/views/SplashView.vue";
+import LoginView from "@/views/LoginView.vue";
+import DashboardView from "@/views/DashboardView.vue";
+import TerminalView from "@/views/TerminalView.vue";
+import DockerView from "@/views/DockerView.vue";
+import ExplorerView from "@/views/ExplorerView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'splash',
-    component: SplashView
+    path: "/",
+    name: "splash",
+    component: SplashView,
   },
   {
-    path: '/login',
-    name: 'login',
-    component: LoginView
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
   {
-    path: '/home',
-    name: 'home',
+    path: "/home",
+    name: "home",
     component: HomeView,
     props: true,
     children: [
       {
-        path: '/dashboard',
+        path: "/dashboard",
         component: DashboardView,
       },
       {
-        path: '/explorer',
+        path: "/explorer",
         component: ExplorerView,
       },
       {
-        path: '/terminal',
+        path: "/terminal",
         component: TerminalView,
       },
       {
-        path: '/docker',
+        path: "/docker",
         component: DockerView,
       },
-    ]
+    ],
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const publicPages = ["/login", "/"];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem("host");
+  console.log("authRequired", authRequired);
+  console.log("loggedIn", loggedIn);
+
+  if (authRequired && !loggedIn) {
+    next("/login");
+  } else if (!authRequired && loggedIn) {
+    next("/dashboard");
+  } else {
+    next();
+  }
+});
+
+export default router;
